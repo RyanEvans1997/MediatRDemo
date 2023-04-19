0db810e2aa77fd8b7edc6a37986a47a73e63702a
RyanE <ryanevans19971@gmail.com>
2023-04-19 16:12:12 +0100

    Added WebAPI project

---
+++ DemoApi/Controller/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controller
+{
+    public class PersonController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PersonController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<List<PersonModel>> Get()
+        {
+            return await _mediator.Send(new GetPersonListQuery());
+        }
+    }
+}
---
+++ DemoApi/Program.cs
@@ -0,0 +1,39 @@
+using DemoLibrary;
+using DemoLibrary.DataAccess;
+
+namespace DemoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddSingleton<IDemoDataAccess, DemoDataAccess>();
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(DemoLibraryMediatREntryPoint).Assembly));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
